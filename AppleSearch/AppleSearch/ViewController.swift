211f425f9854131f28ad9824e508bafbb3ed67aa
Trevor Bursach <bursacht@gmail.com>
2020-09-24 10:18:55 -0600

    Initial commit

---
+++ AppleSearch/AppleSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleSearch
+//
+//  Created by Trevor Bursach on 9/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
